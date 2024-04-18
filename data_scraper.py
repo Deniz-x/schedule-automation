c706a7b50f47314b79424f738917492f431c6ffb
Melek Deniz Colak <deniz@Meleks-MacBook-Air.local>
2024-04-18 19:46:53 -0400

    makes today's schedule

--- data_scraper.py
+++ data_scraper.py
@@ -7,120 +7,69 @@ from bs4 import BeautifulSoup
 from datetime import date, timedelta
 import time
 
+def get_reservations():
+    # configure webdriver
+    options = Options()
+    # options.add_argument("--headless=new")
+    options.add_argument("--window-size=1920,1080")  # set window size to native GUI size
+    options.add_argument("start-maximized")  # ensure window is full-screen
 
-# configure webdriver
-options = Options()
-# options.add_argument("--headless=new")
-options.add_argument("--window-size=1920,1080")  # set window size to native GUI size
-options.add_argument("start-maximized")  # ensure window is full-screen
+    ...
+    driver = webdriver.Chrome(options=options)
+    driver.get("https://amlf.cs.mcgill.ca/Web/view-schedule.php?")
 
-...
-driver = webdriver.Chrome(options=options)
-#     https://amlf.cs.mcgill.ca/Web/view-schedule.php?sd=2024-1-8                    ^^^^^^^^^^^^^^^
-#driver.get("https://amlf.cs.mcgill.ca/Web/view-schedule.php?")
-driver.get("https://amlf.cs.mcgill.ca/Web/view-schedule.php?sd=2024-1-8")
-# wait for page to load
-#element = WebDriverWait(driver=driver, timeout=5).until(
-#    EC.presence_of_element_located((By.CSS_SELECTOR, 'div[data-target=directory-first-item]'))
-#)
-
-soup = BeautifulSoup(driver.page_source, 'lxml')
-#css_selector = f"a[id={date.today()}] ~ .reservations"
-date_range_on_page = soup.find('button', class_="change-date").findNext("div").text.strip().split("-")
-first_date_on_page = date_range_on_page[0].strip()
-last_date_on_page = date_range_on_page[1].strip().split("/")
-print(last_date_on_page)
-last_date_year = int("20" + last_date_on_page[2])
-last_date_month = int(last_date_on_page[0])
-last_date_day = int(last_date_on_page[1])
-last_date = date(last_date_year, last_date_month, last_date_day)
-
-print(f"last date on page is {last_date}")
-driver.implicitly_wait(10)
-#todays_reservation_table = soup.find('a', id=f"{date.today()+timedelta(days=5)}").findNext("table")
-#print(f"today's reservation table is: {todays_reservation_table}")
-#todays_reservations = todays_reservation_table.find_all("div", class_="reserved")
-
-#print(f"today's reservations are: {todays_reservations}")
-
-# Wait for the table to be present
-table_locator = (By.ID, f"{date.today() + timedelta(days=5)}")
-WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located(table_locator)
-)
-#get today's reservation table
-todays_reservation_table = soup.find('a', id=f"{date.today() + timedelta(days=5)}").findNext("table")
-
-# Wait for the reservations to be present
-reservations_locator = (By.CLASS_NAME, "reserved")
-WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located(reservations_locator))
-#get the reservations
-todays_reservations = todays_reservation_table.find_all("div", class_="reserved")
-
-print(f"today's reservations are: {todays_reservations}")
-
-
-#if there are no reservations today, find a day with reservations
-current_lookup_date = date.today()+timedelta(days=5) + timedelta(days=1)
-if len(todays_reservations) == 0:
-    i = 1
-    while i<7:
-        time.sleep(1)
-        
-        #print("----------------------")
-        #print(f"next_reservation table on {current_lookup_date} is {next_reservation_table.prettify()}")
-        #print("----------------------")
-        table_locator = (By.ID, f"{current_lookup_date}")
-        todays_reservation_table = WebDriverWait(driver, 10).until(EC.presence_of_element_located(table_locator))
-        next_reservation_table = soup.find('a', id=f"{current_lookup_date}").findNext("table")
-
-        # Wait for the reservations to be present
-        reservations_locator = (By.XPATH, "//div[@class='reserved']")
-        todays_reservations = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located(reservations_locator))
-        next_reservations = next_reservation_table.find_all("div", class_="reserved pending event")
-        #next_reservations = next_reservation_table.select("div.reserved, div.reserved.pending.event")
-        
-        print(f"next_reservations on {current_lookup_date} is {next_reservations}")
-
-        i += 1
-
-        if current_lookup_date==last_date:
-            print("No reservations this week!")
-            break
-
-        current_lookup_date += timedelta(days=1)
-
-        if len(next_reservations) != 0:
-            break
-        
-
-
-reservations = []
-
-for reservation in todays_reservations:
-    reservation_info = {}
-    reservation_info["title"] = reservation.get_text()
-
-    reservation_addresss = reservation['data-resid']
-    driver.get(f"https://amlf.cs.mcgill.ca/Web/reservation/?rn={reservation_addresss}")
-    time.sleep(5)
-
-    soup2 = BeautifulSoup(driver.page_source, 'lxml')
-    reservation_owner = soup2.select_one("#reservation-owner-section .reservation-content-section .reservation-label").findNext("div")
-    reservation_info["owner"] = reservation_owner.get_text()
-    res_time = soup2.select("#reservation-times-section .reservation-content-section .reservation-label")
-    start_time = res_time[0].findNext("div")
-    end_time = res_time[1].findNext("div")
-    print(f"start time is {start_time.get_text()}")
-    reservation_info["start_time"] = start_time.get_text().split(", ")[1]
-    reservation_info["end_time"] = end_time.get_text().split(", ")[1]
-    lab = soup2.select_one("#reservation-resources-section .reservation-selected-resources .reservation-resource").get_text()
-    reservation_info["lab"] = lab
-    instructor = soup2.select_one("#reservation-details-section .row .attribute .attribute-value").get_text()
-    reservation_info["instructor"] = instructor
-    reservations.append(reservation_info)
+    soup = BeautifulSoup(driver.page_source, 'lxml')
     
+    driver.implicitly_wait(10)
     
-print(reservations)
-# the pattern for the reservation link: https://amlf.cs.mcgill.ca/Web/reservation/?rn=56BC9F6B
-# data-resid
+    #get today's reservation table
+    todays_reservation_table = soup.find('a', id=f"{date.today()}").findNext("table")
+
+    # Wait for the reservations to be present
+    reservations_locator = (By.CLASS_NAME, "reserved")
+    WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located(reservations_locator))
+    #get the reservations
+    todays_reservations = todays_reservation_table.find_all("div", class_="reserved")
+
+    reservations = []
+    # for each reservation, the relevant reservation info is extracted into a dictionary which is saved in the reservations list
+    for reservation in todays_reservations:
+        reservation_info = {}
+        reservation_info["title"] = reservation.get_text().strip()
+
+        # the pattern for the reservation link: https://amlf.cs.mcgill.ca/Web/reservation/?rn=56BC9F6B
+        reservation_addresss = reservation['data-resid']
+        driver.get(f"https://amlf.cs.mcgill.ca/Web/reservation/?rn={reservation_addresss}")
+        time.sleep(5)
+
+        soup2 = BeautifulSoup(driver.page_source, 'lxml')
+        reservation_owner = soup2.select_one("#reservation-owner-section .reservation-content-section .reservation-label").findNext("div")
+        reservation_info["owner"] = reservation_owner.get_text()
+
+        #get the reservation time block
+        res_time = soup2.select_one("#reservation-times-section")
+
+        res_time_content = res_time.select_one(".reservation-content-section")
+
+        #get start time
+        start_time_div = res_time_content.select_one("div:first-child")
+        start_time_list = start_time_div.select_one("div:nth-child(2)").get_text().split(" ")[1:]
+        start_time = ' '.join(start_time_list)
+
+        #get end time
+        end_time_div = start_time_div.find_next_sibling("div")
+        end_time_list = end_time_div.select_one("div:nth-child(2)").get_text().split(" ")[1:]
+        end_time = ' '.join(end_time_list)
+
+        #add the times to dictionary
+        reservation_info["start_time"] = start_time
+        reservation_info["end_time"] = end_time
+
+        lab = soup2.select_one("#reservation-resources-section .reservation-selected-resources .reservation-resource").get_text()
+        reservation_info["lab"] = lab
+        instructor = soup2.select_one("#reservation-details-section .row .attribute .attribute-value").get_text()
+        reservation_info["instructor"] = instructor
+        reservations.append(reservation_info)
+        
+        
+    return reservations
--- schedule_builder.py
+++ schedule_builder.py
@@ -1,24 +1,85 @@
 #from data_scraper import reservations
 from pptx import Presentation
+from data_scraper import get_reservations
+from pptx.dml.color import RGBColor  # Import RGBColor explicitly
+from pptx.util import Pt
+from pptx.enum.text import PP_ALIGN
+from datetime import datetime
+
+#get reservations using the data_scraper
+reservations = get_reservations()
+
 
 template_path = './template.pptx'
 prs = Presentation(template_path)
 
+
 slide = prs.slides[0]
+
+#putting today's date on top of the table
+date_shape = slide.shapes[2]
+today_date = datetime.now()
+today_date_string = today_date.strftime("%A, %Y-%m-%d")
+date_shape.text_frame.clear()
+p = date_shape.text_frame.add_paragraph()
+p.text = today_date_string
+p.font.size = Pt(60) 
+p.font.name = 'Arial' 
+p.alignment = PP_ALIGN.CENTER
+p.font.bold = True 
 
 # Loop through shapes in the slide and find the table
 for shape in slide.shapes:
     if shape.has_table:
         table = shape.table
+        print("found the table!")
         break  # Assuming there's only one table on the slide
 
 # Get the number of rows and columns in the table
 num_rows = len(table.rows)
 num_cols = len(table.columns)
 
-# Loop through columns first
-for col_idx in range(1,num_cols):
-    for row_idx in range(1,num_rows):
-        cell = table.cell(row_idx, col_idx)
-        # Access and modify cell content as needed
-        print(cell.text)
+#dictionary to map the time to a specific cell location
+time_to_cell_dict = {"8:30 AM": 1, "9:00 AM": 2, "9:30 AM": 3, "10:00 AM": 4, "10:30 AM": 5, "11:00 AM": 6, "11:30 AM": 7, "12:00 PM": 8,
+                    "12:30 PM": 9, "1:00 PM": 10, "1:30 PM": 11, "2:00 PM": 12, "2:30 PM": 13, "3:00 PM": 14, "3:30 PM": 15, "4:00 PM": 16,
+                    "4:30 PM": 17, "5:00 PM": 18}
+
+lab_to_cell_dict = {"Lab 1": 1, "Lab 2": 2, "Lab 3": 3}
+
+#loop through the reservations and put them on the table
+for reservation in reservations:
+    #handle the case where reservations start time or end time is after 5:00 PM - idea use a python representation of hour and say if time is later than 5 pm
+    
+    if reservation["start_time"] not in time_to_cell_dict:
+        top_cell_index = 18
+    else:
+        top_cell_index = time_to_cell_dict[reservation["start_time"]]
+
+    
+    if reservation["end_time"] not in time_to_cell_dict:
+        bottom_cell_index = 18
+    else:
+        bottom_cell_index = time_to_cell_dict[reservation["end_time"]]
+
+
+    top_cell = table.cell(top_cell_index, lab_to_cell_dict[reservation["lab"]])
+    bottom_cell = table.cell(bottom_cell_index, lab_to_cell_dict[reservation["lab"]])
+
+    top_cell.merge(bottom_cell)
+    merged_cell = table.cell(top_cell_index, lab_to_cell_dict[reservation["lab"]])
+
+    p = merged_cell.text_frame.add_paragraph()
+    p.text = f"{reservation["title"]}\n{reservation["start_time"]} to {reservation["end_time"]}"
+
+    p.font.size = Pt(30) 
+    p.font.name = 'Arial' 
+    p.font.bold = True  
+    p.alignment = PP_ALIGN.CENTER
+
+    #color the cell
+    fill = merged_cell.fill
+    fill.solid()
+    fill.fore_color.rgb = RGBColor(244,13,48)
+
+    
+prs.save("schedule_of_the_day2.pptx")
